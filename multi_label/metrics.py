5abf5709f8f32801b2b1abc2f50aebca46ffb241
junhyuk <jh_lee@g.kmou.ac.kr>
2021-11-22 14:13:28 +0000

    [FEAT] change metric to mean metric

--- multi_label/metrics.py
+++ multi_label/metrics.py
@@ -6,30 +6,33 @@ def accuracy(pred: torch.Tensor, ground_truth: torch.Tensor) -> float:
     return torch.mean((pred == ground_truth).to(float)).item()
 
 
-def overall(pred: torch.Tensor, ground_truth: torch.Tensor, option , label_count: int = 39):
+def overall(pred: torch.Tensor, ground_truth: torch.Tensor, option , label_count: int = 39) -> float:
     assert pred.shape == ground_truth.shape
     assert option in ['recall', 'precision']
 
-    pf, gf = pred.flatten(), ground_truth.flatten()
-
-    confusion_matrix = {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
-    for i, (predict, ground) in enumerate(zip(pf, gf)):
-        if ground:
-            if predict == ground:
-                confusion_matrix['tp'] += 1
+    metric = []
+    for i in range(pred.shape[-1]):
+        confusion_matrix = {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
+        for j in range(pred.shape[0]):
+            g, p = ground_truth[j, i], pred[j, i]
+            if g:
+                if p == g:
+                    confusion_matrix['tp'] += 1
+                else:
+                    confusion_matrix['fn'] += 1
             else:
-                confusion_matrix['fn'] += 1
-        else:
-            if predict == ground:
-                confusion_matrix['tn'] += 1
-            else:
-                confusion_matrix['fp'] += 1
+                if p == g:
+                    confusion_matrix['tn'] += 1
+                else:
+                    confusion_matrix['fp'] += 1
 
-    if option == 'recall':
-        return confusion_matrix['tp'] / (confusion_matrix['tp'] + confusion_matrix['fn'])
-        
-    if option == 'precision':
-        return confusion_matrix['tp'] / (confusion_matrix['tp'] + confusion_matrix['fp'])
+            if option == 'recall':
+                metric.append(confusion_matrix['tp'] / (confusion_matrix['tp'] + confusion_matrix['fn']))
+                
+            if option == 'precision':
+                metric.append(confusion_matrix['tp'] / (confusion_matrix['tp'] + confusion_matrix['fp']))
+
+    return np.mean(metric) 
 
 
 
