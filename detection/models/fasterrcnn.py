885caa91b90bec1293e50d6da3205ae78f2c31ed
JiyouSeo <seojiyu1113@gmail.com>
2021-12-01 11:38:30 +0000

    [FEAT] early stopping, mAP code add

--- detection/models/fasterrcnn.py
+++ detection/models/fasterrcnn.py
@@ -5,7 +5,7 @@ from pytorch_lightning import LightningModule
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.ops import box_iou
-
+from models.metrics import mean_average_precision
 
 def _evaluate_iou(target, pred):
     """Evaluate intersection over union (IOU) for target from dataset and output prediction from model."""
@@ -32,7 +32,8 @@ class LitModel(LightningModule):
                                 num_classes=num_classes,
                                 rpn_anchor_generator=self.anchor_generator,
                                 box_roi_pool=self.roi_pooler)
-    
+
+
     def forward(self, imgs):
         self.model.eval()
         return self.model(imgs)
@@ -41,20 +42,41 @@ class LitModel(LightningModule):
         # "batch" is the output of the training data loader.
         imgs, targets = batch
         loss_dict = self.model(imgs, targets)
-        # print(loss_dict)
         loss = sum(loss for loss in loss_dict.values())
         return {"loss": loss, "log": loss_dict}
+    
+    def training_epoch_end(self, outs):
+        loss_sum = torch.stack([o["loss"] for o in outs]).sum()
+        self.log('loss_sum', loss_sum)
 
     def validation_step(self, batch, batch_idx):
         imgs, targets = batch
         outs = self.model(imgs)
-        print(outs)
+
+        step_mAP = 0.
+        for j,o in enumerate(outs):
+            pred_boxes = o['boxes']
+            pred_labels = o['labels']
+            pred_scores = o['scores']
+            pred_idx = torch.tensor([i for i in range(len(pred_labels))], device=pred_labels.device)
+
+            target_boxes = targets[j]['boxes']
+            target_labels = targets[j]['labels']
+            target_idx = torch.tensor([i for i in range(len(target_labels))], device=target_labels.device) 
+
+            iou_thres = 0.1
+            mAP = mean_average_precision(pred_idx, pred_scores, pred_labels, pred_boxes, target_idx, target_labels, target_boxes, iou_thres)
+            step_mAP += mAP.float()
+        step_mAP /= len(outs)
         iou = torch.stack([_evaluate_iou(t, o) for t, o in zip(targets, outs)]).mean()
-        return {"val_iou": iou}
+        return {"val_iou": iou, "val_mAP": step_mAP}
 
     def validation_epoch_end(self, outs):
+        total_mAP = torch.stack([o["val_mAP"] for o in outs]).mean()
         avg_iou = torch.stack([o["val_iou"] for o in outs]).mean()
         logs = {"val_iou": avg_iou}
+        self.log("val_iou", avg_iou)
+        self.log("total_mAP", total_mAP)
         return {"avg_val_iou": avg_iou, "log": logs}
     
     def configure_optimizers(self):
--- detection/train.py
+++ detection/train.py
@@ -1,23 +1,23 @@
 import os
-import torch
-import torch.nn as nn
-from models.models import LitModel
-from datasets.dataModule import CustomDataModule
+
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+import torch
+import torchvision
+from pytorch_lightning import LightningModule
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+from datasets.dataModule import CustomDataModule
+from models.fasterrcnn import LitModel
 
 
 # Path to the folder where the pretrained models are saved
 CHECKPOINT_PATH = "/opt/ml/finalproject/saved_models/"
-
 # Function for setting the seed
 pl.seed_everything(42)
 
-# Ensure that all operations are deterministic on GPU (if used) for reproducibility
-torch.backends.cudnn.determinstic = True
-torch.backends.cudnn.benchmark = False
-
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
 def train_model(model_name, save_name=None, **kwargs):
     """
@@ -38,36 +38,20 @@ def train_model(model_name, save_name=None, **kwargs):
     trainer = pl.Trainer(
         default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),  # Where to save models
         # We run on a single GPU (if possible)
-        gpus=1 if str(device) == "cuda:0" else 0,
+        gpus=1,
         # How many epochs to train for if no patience is set
-
         max_epochs=180,
-        # callbacks=[
-        #     ModelCheckpoint(
-        #         save_weights_only=True, mode="min", monitor="loss"
-        #     ),  # Save the best checkpoint based on the maximum val_acc recorded. Saves only weights and not optimizer
-        #     LearningRateMonitor("epoch"),
-        # ],  # Log learning rate every epoch
+        callbacks=[
+            EarlyStopping(monitor="total_mAP", patience=5, verbose=False, mode="max")
+             # Save the best checkpoint based on the maximum val_acc recorded. Saves only weights and not optimizer
+        ],  # Log learning rate every epoch
         # progress_bar_refresh_rate=1
     )  # In case your notebook crashes due to the progress bar, consider increasing the refresh rate
-    trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
-    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
         
     pl.seed_everything(42)  # To be reproducable
-    model = LitModel(model_name=model_name, **kwargs)
+    model = LitModel()
     trainer.fit(model, train_loader, val_loader)
-    # model = LitModel.load_from_checkpoint(
-    #     trainer.checkpoint_callback.best_model_path
-    # )  # Load best checkpoint after training
 
-    # Test best model on validation and test set
-    # val_result = trainer.test(model, test_dataloaders=val_loader, verbose=False)
-    # # test_result = trainer.test(model, test_dataloaders=test_loader, verbose=False)
-    # test_result = trainer.test(model, test_dataloaders=val_loader, verbose=False)
-    
-    # result = {"test": test_result[0]["test_acc"], "val": val_result[0]["test_acc"]}
-    # result = {"test": 0, "val": val_result["val_iou"]}
-    # return result
 
 train_model(
     model_name="fcnn",
