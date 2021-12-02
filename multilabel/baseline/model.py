35bf5108fbf071769c91ee813c908232edcf23c1
junhyuk <jh_lee@g.kmou.ac.kr>
2021-12-02 02:57:52 +0000

    [FEAT] clsupgrade_ver

--- multilabel/baseline/model.py
+++ multilabel/baseline/model.py
@@ -8,18 +8,19 @@ class ResNet101(nn.Module):
         super().__init__()
 
         self.backbone = models.resnet101(pretrained=True)
-        self.fc1 = nn.Linear(1000, 500)
-        self.fc2 = nn.Linear(500, 250)
-        self.fc3 = nn.Linear(250, 125)
-        self.fc4 = nn.Linear(125, num_classes)
+        self.backbone.fc = nn.Linear(2048, num_classes)
+        # self.fc1 = nn.Linear(1000, 500)
+        # self.fc2 = nn.Linear(500, 250)
+        # self.fc3 = nn.Linear(250, 125)
+        # self.fc4 = nn.Linear(125, num_classes)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         output = self.backbone(x)
-        output = self.fc1(output)
-        output = self.fc2(output)
-        output = self.fc3(output)
-        output = self.fc4(output)
+        # output = self.fc1(output)
+        # output = self.fc2(output)
+        # output = self.fc3(output)
+        # output = self.fc4(output)
         output = self.sigmoid(output)
         return output
 
