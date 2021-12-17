325609b9a254309318e5814523e09fdb2d981c67
junhyuk <jh_lee@g.kmou.ac.kr>
2021-12-17 13:50:54 +0000

    [FEAT] add numba in preprocess

--- multilabel/API/preprocess.py
+++ multilabel/API/preprocess.py
@@ -3,6 +3,34 @@ import albumentations.augmentations.crops.transforms as T
 import numpy as np
 from PIL import Image
 import cv2
+from numba import njit
+
+@njit
+def get_box(image: np.ndarray):
+    '''
+    이미지를 순회하면서 객체가 들어 있는 가장 큰 box 좌표를 구해주는 코드
+    Args: 
+        image: np.ndarray
+            blur 처리된 image
+    returns:
+        list of box coordinates([xmin, ymin, xmax, ymax])
+    '''
+    xmin, ymin, xmax, ymax = 1e+5, 1e+5, 0, 0
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            pix = sum(image[i, j, :]) / 3
+
+            if pix < 220:
+                if i < ymin: 
+                    ymin = i
+                if j < xmin:
+                    xmin = j
+                if i > ymax:
+                    ymax = i
+                if j > xmax:
+                    xmax = j
+    
+    return [xmin, ymin, xmax, ymax]
 
 class processer():
     def __init__(self, bar_margin = 75, side_margin = 150):
@@ -12,35 +40,6 @@ class processer():
                 T.CenterCrop(1080 - bar_margin, 1920 - (side_margin * 2), always_apply=True),
             ]
         )
-
-    # 많지 않은 샘플이기에, 속도가 많이 느릴것 같지 않아 numba 제외. 
-    # 필요하면 @njit 데코레이터만 넣으면 됨
-    def get_box(self, image: np.ndarray):
-        '''
-        이미지를 순회하면서 객체가 들어 있는 가장 큰 box 좌표를 구해주는 코드
-        Args: 
-            image: np.ndarray
-                blur 처리된 image
-        returns:
-            list of box coordinates([xmin, ymin, xmax, ymax])
-        '''
-        xmin, ymin, xmax, ymax = 1e+5, 1e+5, 0, 0
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                pix = sum(image[i, j, :]) / 3
-
-                if pix < 220:
-                    if i < ymin: 
-                        ymin = i
-                    if j < xmin:
-                        xmin = j
-                    if i > ymax:
-                        ymax = i
-                    if j > xmax:
-                        xmax = j
-        
-        return [xmin, ymin, xmax, ymax]
-
 
     def cropper(self, image):
         '''
@@ -55,7 +54,7 @@ class processer():
         image = meta['image']
 
         b_image = cv2.GaussianBlur(image, ksize=(7,7), sigmaX=0)
-        xmin, ymin, xmax, ymax = list(map(int, self.get_box(b_image)))
+        xmin, ymin, xmax, ymax = list(map(int, get_box(b_image)))
         post_transform = A.Compose([
             T.Crop(xmin, ymin, xmax, ymax, always_apply=True)
         ])
